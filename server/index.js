df75cbeb13aaa202ec439f980b42fc7bd147cc29
Josh <herz.josh@gmail.com>
2019-11-04 18:02:48 -0800

    edited index.js to allow loader io, edited index.html to include endpoint for josh overview

--- server/index.js
+++ server/index.js
@@ -7,7 +7,10 @@ const cors = require('cors');
 
 const app = express();
 
-app.use('/', express.static('public'));
+//app.use('/', express.static('public'));
+//For Loader io below
+app.use('/', express.static(__dirname + '/../')); //loader.io
+//For Loader io above
 app.use('/:gameId', express.static('public'));
 app.use(compression());
 app.use(bodyParser.json());
@@ -15,11 +18,12 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // write code to handle getting data
-// let overview = 'http://ec2-13-57-13-174.us-west-1.compute.amazonaws.com';
+// let overview = 'localhost:3000';
 // let image = 'http://ec2-52-53-185-254.us-west-1.compute.amazonaws.com/';
 // let reviews = 'http://ec2-54-183-55-106.us-west-1.compute.amazonaws.com';
 // let aboutGame = 'http://my-env.wf8cymc59m.us-west-1.elasticbeanstalk.com';
 // let aboutGame = 'http://ec2-18-218-54-252.us-east-2.compute.amazonaws.com:8081';
+
 
 app.use('/api/:service', (req, res) => {
   let service = req.params.service;
